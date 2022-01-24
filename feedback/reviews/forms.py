30d3d120bce2f15d7ded59875b3b7e9ad07d02f6
Priyanka Jaiswal <jaiswal.priyanka181294@gmail.com>
2022-01-24 19:00:04 +0530

    Feedback Project: Forms - Class Views

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -3,6 +3,17 @@ from django import forms
 
 from .models import Review
 
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label="First Name", max_length="100", error_messages={
+#         "required": "First Name can't be empty!",
+#         "max_length": "Please enter a shorter name!"
+#     })
+#     last_name = forms.CharField(label="Last Name", required="False", max_length="100", error_messages={
+#         "max_length": "Please enter a shorter name!"
+#     })
+#     review = forms.CharField(label="Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Rating", min_value=1, max_value=5) 
+
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,24 +1,49 @@
+from tempfile import template
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
+    
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
+    
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+class ReviewView(CreateView):
+    model = Review
+    # field = "__all__"
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+    
 def update_review(request, id):
     if request.method == 'POST':
         existing_entry = Review.objects.get(pk=id)
@@ -34,5 +59,39 @@ def update_review(request, id):
         "form": form
     })
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This work!"
+        return context
+
+# class ReviewsListView(TemplateView):
+#     template_name = "reviews/review_list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["all_reviews"] = Review.objects.all()
+#         return context
+
+# class ReviewDetailView(TemplateView):
+#     template_name = "reviews/review_detail.html"
+    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["review"] = Review.objects.get(pk=kwargs["id"])
+#         return context
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "all_reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     return base_query.filter(rating__gt=4)
+
+class ReviewDetailView(DetailView):
+    template_name = "reviews/review_detail.html"
+    model = Review
